7eaa1a7623e9b1c1d65d689053361c198025c720
nevermoreluo <656763371@qq.com>
2016-04-18 16:32:04 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#_author = Nevermore_luo
+
+
+
+
+
+#收件人邮箱地址，群发请用逗号隔开,ex: 'xxxx@qq.com,zzz@126.com' 
+To = ''
+#发件人邮箱地址 
+From='xxx@126.com'
+#密码 
+Password='546546'
+#发件人邮箱的SMTP服务器地址 
+#126:'smtp.126.com'\163:'smtp.163.com'\qq:'smtp.qq.com'\sina:'smtp.sina.com'\gmail:'smtp.gmail.com' 
+SMTPServer='smtp.126.com'
+#服务器端口 
+SMTPPort=25
+
+#图片数量 
+Picture_quantity = 10
+#是否保存图片，True/False 
+Save_pic = True
+#保存位置(请务必保留引号前面的r) 
+Dirname = r'c:\xxx'
